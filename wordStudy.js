18e7dce6f909b129a21a7b5bd556c1e5a7b6ebeb
donqlee <donqlee88@gmail.com>
2022-04-17 13:45:05 +0900

    백준 1157

--- wordStudy.js
+++ wordStudy.js
@@ -1,9 +1,32 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-let input = fs.readFileSync(filePath).toString().toUpperCase();
+let input = fs.readFileSync(filePath).toString().trim().toLowerCase();
+const charMap = {};
 
-console.log(input);
+for (let char of input){
+    charMap[char] = charMap[char] ? charMap[char] + 1 : 1;
+}
+let max = Math.max.apply(null, Object.values(charMap));
+let maxChar = '';
+let count = 0;
+for(let char in charMap){
+    if(charMap[char] === max){
+        maxChar = char;
+        count++;
+    }
+}
+
+if(count > 1){
+    console.log('?');
+}else{
+    console.log(maxChar.toUpperCase());
+}
 
 // const result = new Array(26).fill(0);
+
+// for (let i = 0; i < input.length; i++) {
+//     console.log(input.charCodeAt(i));
+//   result[input.charCodeAt(i) - 97] ++;
+// }
 
 // console.log(result);
